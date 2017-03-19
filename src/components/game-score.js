52c47dd5e0d00aa30e8488fcf4139631065f00da
Douglas Moore <douglas@bloglovin.com>
2017-03-19 19:54:06 -0400

    Style game

--- src/components/game-score.js
+++ src/components/game-score.js
@@ -4,7 +4,7 @@ class GameScore extends React.Component {
   render () {
     return (
       <div className='game-score'>
-        {this.props.score}
+        {`Total Score: ${this.props.score}`}
       </div>
     );
   }
